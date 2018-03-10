1747d4eb836934c75d6632a9385b11d16e0d0403
Agnieszka Zuchora <azuchora55@gmail.com>
2018-03-10 16:57:39 +0100

    js obliczenia funkcji

--- warsztaty/javascript/js/funkcje.js
+++ warsztaty/javascript/js/funkcje.js
@@ -13,3 +13,20 @@ function wyswietlWKonsoliParami ( imie, nazwisko ) {
 wyswietlWKonsoliParami( "Agnieszka", "Zuchora" );
 wyswietlWKonsoliParami( "Agnieszka", "Zuchora" );
 wyswietlWKonsoliParami( "Agnieszka", "Zuchora" );
+
+
+//zwracanie wartosci przez funkcje
+
+function obliczPodatek( kwota, procentPodatek ){
+    var wynik = kwota * ( procentPodatek / 100 );
+    return wynik.toFixed (2);
+}
+
+var obliczenia1 = obliczPodatek( 4990, 23);
+console.log( "Wysokość obliczonego podatku wynosi: " + obliczenia1 );
+
+var w = obliczPodatek( 1000, 10 );
+var x = obliczPodatek( 4000, 10 );
+var z = obliczPodatek( 1000, 10 );
+
+console.log(w,x,z);
