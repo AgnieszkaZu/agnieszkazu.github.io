8936d84cfb6cedbd80b6afc2f6630c238964e785
Agnieszka Zuchora <azuchora55@gmail.com>
2018-03-10 15:51:16 +0100

    js console log

---
+++ warsztaty/javascript/js/zmienne.js
@@ -0,0 +1,18 @@
+//console.log ("agnieszka");
+
+var imie;
+var nazwisko = "Zuchora";
+var wiek = 38;
+
+//console.log(imie);
+//
+//imie = "Agnieszka";
+//
+//console.log(imie);
+
+imie = "Agnieszka";
+
+
+//laczenie tekstu
+var laczenieTekstu = "Moje imie to: " + imie + " a nazwisko to: " + nazwisko;
+console.log(laczenieTekstu);
