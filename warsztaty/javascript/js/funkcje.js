e73d9b142c5ff7032010a82333d790fa64d2c00d
Agnieszka Zuchora <azuchora55@gmail.com>
2018-03-10 16:32:23 +0100

    js wyswietlanie funkcji z parametrami

---
+++ warsztaty/javascript/js/funkcje.js
@@ -0,0 +1,15 @@
+// funkcja bez parametrow
+function wyswietlWKonsoli() {
+    console.log("AgnieszkaZuchora");
+}
+
+wyswietlWKonsoli();
+
+//funkcja z parametrami
+
+function wyswietlWKonsoliParami ( imie, nazwisko ) {
+    console.log( imie + " " + nazwisko );
+}
+wyswietlWKonsoliParami( "Agnieszka", "Zuchora" );
+wyswietlWKonsoliParami( "Agnieszka", "Zuchora" );
+wyswietlWKonsoliParami( "Agnieszka", "Zuchora" );
