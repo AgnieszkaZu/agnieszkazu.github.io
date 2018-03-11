641de9c8660742abe49ebf181bb6ae747e2a6a7e
Agnieszka Zuchora <azuchora55@gmail.com>
2018-03-11 14:48:51 +0100

    js operatory logiczne

---
+++ warsztaty/javascript/js/operatory.js
@@ -0,0 +1,87 @@
+//// operatory arytmentyczne
+//var liczba1 = 20;
+//var liczba2 = 4;
+//
+//// dodawanie
+//var wynik = liczba1 + liczba2;
+//
+//// odejmowanie
+//wynik = liczba1 - liczba2;
+//
+//// mnożenie
+//wynik = liczba1 * liczba2;
+//
+//// dzielenie
+//wynik = liczba1 / liczba2;
+//
+////modulo
+//wynik = liczba1 % liczba2;
+//
+//console.log( wynik );
+//
+////inkrementacja
+//liczba2 = liczba2 + 1; //liczba2++
+////liczba 2 + 1
+//liczba2 = liczba2 -1; //liczba2--
+//
+//liczba2++;
+//liczba2--;
+//
+//console.log( liczba2 );
+//
+//
+//
+//
+//
+//
+////operatory przypisania
+//var liczba3 = 200;
+//var liczba4 = 100;
+//var liczba5 = 15;
+//var liczba6 = 40;
+//var liczba7 = 7
+//
+//liczba3 +=200; //liczba3 = liczba3 + 200;
+//liczba4 -=50; //liczba4 = liczba4 - 50;
+//liczba5 *=2; //liczba5 = liczba5 * 2
+//liczba6 /=2 // liczba6 = liczba 6 / 2;
+//liczba7 %=2; // liczba7 = liczba7 % 2;
+//
+//console.log( liczba7 );
+//
+//
+//
+//
+//
+//
+//// operatory porownania
+//var liczba8 = 42;
+//var liczba9 = 10;
+//
+//if(liczba8 == liczba9) {
+//    console.log( "Warunek spełniony, czyli true" );
+//} else {
+//    console.log( "Warunek nie spełniony, czyli false" );
+//}
+//
+//if(liczba8 != liczba9) {
+//    console.log( "Warunek spełniony, czyli true" );
+//} else {
+//    console.log( "Warunek nie spełniony, czyli false" );
+//}
+//
+//
+//
+
+//operator logiczny
+
+var liczba10 = 20;
+var liczba11 = 4;
+var czyWiosna = "tak";
+
+if( liczba10 < liczba11 && czyWiosna == "tak" ) {
+    console.log( "Warunek spełniony" );
+} else {
+    console.log( "Warunek nie spełniony" );
+}
+
