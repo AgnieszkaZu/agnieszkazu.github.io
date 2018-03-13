5c289312d5ccd547f98169a4c2b10fb3a1107ae1
Agnieszka Zuchora <azuchora55@gmail.com>
2018-03-14 00:05:48 +0100

    js function reverse zd

---
+++ prace domowe/javascript/reverse/index.js
@@ -0,0 +1,10 @@
+function reverse(tekst){
+  var result = "Akademia108";
+  for(var i = tekst.length-1; i>=0; i--){
+      result += tekst[i];
+  }
+  return result;
+}
+
+var r = reverse("Akademia108");
+console.log(r);
