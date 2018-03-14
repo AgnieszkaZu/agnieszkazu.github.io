08300611976404c504e864f65a9e2f13593d273f
Agnieszka Zuchora <azuchora55@gmail.com>
2018-03-14 22:54:09 +0100

    js fibonacci zd

--- prace domowe/javascript/reverse/index.js
+++ prace domowe/javascript/reverse/index.js
@@ -1,10 +1,18 @@
-    function reverse(tekst) {
-        var result = "Akademia108";
-        for (var i = tekst.length - 1; i >= 0; i--) {
-            result += tekst[i];
-        }
-        return result;
-    }
+//    function reverse(tekst) {
+//        var result = "Akademia108";
+//        for (var i = tekst.length - 1; i >= 0; i--) {
+//            result += tekst[i];
+//        }
+//        return result;
+//    }
+//
+//    var r = reverse("Akademia108");
+//    console.log(r);
 
-    var r = reverse("Akademia108");
-    console.log(r);
+// fibonacci
+
+var fibonacci = [0, 1];
+for (var i = fibonacci.length; i < 20; i++) {
+    fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+}
+console.log(fibonacci);
