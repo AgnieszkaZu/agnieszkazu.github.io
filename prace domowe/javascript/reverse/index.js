ee903a05e1d0c8468e829c71d048891c16b117c7
Agnieszka Zuchora <azuchora55@gmail.com>
2018-03-14 23:45:31 +0100

    sortowanie liter stringa

--- prace domowe/javascript/reverse/index.js
+++ prace domowe/javascript/reverse/index.js
@@ -16,3 +16,21 @@ for (var i = fibonacci.length; i < 20; i++) {
     fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
 }
 console.log(fibonacci);
+
+
+
+function sortujString(napis) {
+    var tekst = napis;
+    var string = [];
+    string = tekst.split("");
+    string.sort();
+    console.log(string.join(""));
+}
+
+sortujString("Akademia108");
+
+
+function sortString(napisZPosortowanymiLiterami) {
+    return napisZPosortowanymiLiterami.split("").sort().join("");
+}
+console.log(sortString("Akademia108"));
