7edbf966452391a071614001c2ced2f970a28490
Agnieszka Zuchora <azuchora55@gmail.com>
2018-03-15 23:32:25 +0100

    js sumaKwadratow zd

--- prace domowe/javascript/reverse/index.js
+++ prace domowe/javascript/reverse/index.js
@@ -53,3 +53,23 @@ var tablica = [1, 2, 3, 4, 5, 6];
 sumaIloczyn(tablica);
 
 //Napisz funkcję ze zmiennymi lokalnymi, która policzy sumę i iloczyn elementów tablicy [1, 2, 3, 4, 5, 6]. Funkcja przyjmuje jeden parametr - ww. tablicę z liczbami. Funkcja nie zwraca nic. Funkcja wyświetla wyniki w konsoli.
+
+//Napisz funkcję ze zmiennymi lokalnymi, która liczy sumę kwadratów elementów tablicy [0,1,2,3,4,5]. Funkcja przyjmuje jeden parametr - w/w tablicę z liczbami. Funkcja zwraca sumę kwadratow. Wyświetl wynik działania funkcji w konsoli.
+//
+//Zadanie domowe robimy z wykorzystaniem serwisu https://repl.it
+//
+//Podpowiedź przed wysłaniem zadania do sprawdzenia:
+//- użyj w zadaniu pętli, którą uważasz za słuszną i klasy Math.
+
+
+function sumaKwadratow(tablicaLiczb) {
+
+    var sumaKwadratowa = 0;
+    for (var i = 0; i < tablicaLiczb.length; i++) {
+        sumaKwadratowa += Math.pow(tablicaLiczb[i], 2);
+    }
+    console.log("Suma kwadratow elementow tablicy wynosi " + sumaKwadratowa);
+
+}
+
+sumaKwadratow([1, 2, 3, 4, 5]);  
