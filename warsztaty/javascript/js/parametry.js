321a89398422dde26222f8192f8cf025405c6a1a
Agnieszka Zuchora <azuchora55@gmail.com>
2018-03-17 09:51:39 +0100

    js parametry

---
+++ warsztaty/javascript/js/parametry.js
@@ -0,0 +1,14 @@
+var liczby = [1,2,3,8,15];
+//function pokazTablice( liczby ){
+//    console.log( );
+}
+
+function pokazTablice( arr == liczby ){
+    console.log( arr );
+pokazTablice ( liczby );
+    
+    pokazTablice( ['a', 'b', 'c',]);
+
+    
+//    nazwa parametru ( arr, zmienna1 ) jest dowolna (z kilkoma ograniczeniami)
+    //nie należy zapisywać parametrow jak zmiennych
