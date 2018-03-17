ae487863998608edda46c28dc49ba1db624d9b16
Agnieszka Zuchora <azuchora55@gmail.com>
2018-03-17 16:30:19 +0100

    js zdarzenia DOM

---
+++ warsztaty/dom/js/zdarzenia.js
@@ -0,0 +1,61 @@
+var paragraf = document.getElementById('par2');
+
+//// sposob 1
+//function obsluzZdarzenie(event) {
+////    consloe.log("clik");
+//    console.log( event.type );
+//}
+//
+////bez nawiasow zapis 
+//paragraf.onclick = obsluzZdarzenie;
+//
+//// sposob 2
+//paragraf.onclick = function(e) {
+//    console.log( "Do obsługi zdarzenia przeypisana funkcja anonimowa" );
+//}
+
+//var counter = 0;
+//paragraf.addEventListener("mouseenter", function(event) {
+//                        counter++;
+//                          console.log( event.type );
+//    console.log( "Zdarzenie wywołane    " + counter + " raz");
+//});
+//
+//var title = document.getElementById("main-header");
+//
+//title.addEventListener("mouseenter", function(){
+//    title.style.fontSize = "70px";
+//    title.style.color = "red";
+//});
+//
+//title.addEventListener("mouseleave", function(){
+//    title.style.fontSize = "initial";
+//    title.style.color = "initial";
+//});
+
+
+//function bodyColor (event) {
+//    console.log(event);
+//    document.body.style.backgroundColor = "red";
+//}
+//
+//var title = document.getElementById("main-header");
+//
+//title.addEventListener("click", bodyColor);
+
+//usuwanie obslugi zdarzen
+//title.removeEventListener("click", bodyColor);
+
+
+//blokowanie domyślnego działania elementow
+
+//document.querySelector('.link1').addEventListener('click', function(event){
+//    event.preventDefault();
+//    console.log("Kliknąłem link klasy .link1")
+//});
+//
+
+
+
+
+
