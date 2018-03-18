41e9dfac074f9f571aa70a2fbb45555bd6797e65
Agnieszka Zuchora <azuchora55@gmail.com>
2018-03-18 11:37:49 +0100

    jQuery document.ready

---
+++ warsztaty/javascript/warsztaty na zajeciach/ustawtlo.js
@@ -0,0 +1,29 @@
+//warsztat1
+//function ustawTlo(){
+//    var akapity = document.getElementsByTagName("p");
+//    akapity[0].style.backgroundColor = "red";
+//    akapity[1].style.backgroundColor = "yellow";
+//    }
+
+
+
+
+//warsztat2
+
+//function pobierzImie(event) {
+//    event.preventDefault();
+//
+//    var input = document.getElementsByTagName("input");
+//
+//    var imie = input[0].value;
+//    var nazwisko = input[1].value;
+//
+//    //    console.log(imie);
+//    //    console.log(nazwisko);
+//    console.log("Imie: " + imie + "\nNazwisko: " + nazwisko);
+//}
+
+
+var input = document.getElementsByTagName("input");
+
+document.querySelectorAll('input[type=checkbox]'
---
+++ warsztaty/jquery/js/main.js
@@ -0,0 +1,9 @@
+//dokument.ready gwarantuje, że skrypty wywołają się w momencie gdy cały model DOM jest już załadowany
+//poniżej skrcocony zapis document.ready
+
+$(function(){
+   
+//    $(".title").css({color : "red", backgroundColor : "yellow"});
+//    $("h2").css({color : "red", backgroundColor : "yellow"});
+    $("[class='title']").css({color : "red", backgroundColor : "yellow"});
+});
