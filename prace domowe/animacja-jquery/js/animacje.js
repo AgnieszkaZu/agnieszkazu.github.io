1c9d48342264577ebcec7a211a3d4ca17fa30842
Agnieszka Zuchora <azuchora55@gmail.com>
2018-03-21 23:51:25 +0100

    prace domowe jQuery

--- prace domowe/animacja-jquery/js/animacje.js
+++ prace domowe/animacja-jquery/js/animacje.js
@@ -1,17 +1,75 @@
+//$(function(){
+//            $("button").click(function () {
+//                    $(".kwadrat").animate({
+//                            'width': "100px",
+//                            'height': "100px",
+//                            'margin-left': "100px"
+//                        }, 3000),
+//                    $(".kwadrat").animate({
+//backgroundColor: "blue"
+//                                }, 5000),
+//
+//                     $(".kwadrat").show(function () {
+//                                    $(this).html('<h2>Animacja zakończona</h2>');
+//
+//                                });
+//            });
+//});
+//$(function () {
+//            $("button").click(function () {
+//                $(".kwadrat").animate({
+//                    'width': "100px",
+//                    'height': "100px",
+//                    'margin-left': "100px"
+//                }, 3000, zmianaKoloru);
+//
+//                function zmianaKoloru() {
+//                    $(".kwadrat").animate({
+//                        backgroundColor: "blue"
+//                    }, 5000, wyskakujacyNapis);
+//                }
+//
+//                function wyskakujacyNapis() {
+//                    $(".kwadrat").html('<h2>Animacja zakończona</h2>');
+//                }
+//            });
+//});
+
+
+
+
 $(function(){
-            $("button").click(function () {
-                    $(".kwadrat").animate({
-                            'width': "100px",
-                            'height': "100px",
-                            'margin-left': "100px"
-                        }, 3000),
-                    $(".kwadrat").css({
-                                    backgroundColor: "blue"
-                                }, 5000),
-
-                     $(".kwadrat").show(function () {
-                                    $(this).html('<h2>Animacja zakończona</h2>');
-
-                                });
+            $("#ostatniNaPierwszy").click(function() {
+                $("p:first").before ($("p:last"));
+            });
+    
+            $("#pierwszyNaOstatni").click(function(){
+                $("p:last").after ($("p:first"));
             });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
